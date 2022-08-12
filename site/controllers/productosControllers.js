b7647fa3376daf1334bf0a1971757c6c6077a176
FrancoSanchez2022 <fraannsanchez40@gmail.com>
2022-08-12 09:41:40 -0300

    subo cambios de la rama admin-edit

--- site/controllers/productosControllers.js
+++ site/controllers/productosControllers.js
@@ -2,18 +2,26 @@ let aside = [
     {
         imagen:"difusor.webp",
         titulo:"Difusor automatico",
+        descripcion:"Aromatizante de ambientes con fragancia bebe",
+        precio: "$216.00"
     },
     {
         imagen:"aqua fresh auto sport.jpg",
         titulo:"Aromatizante de auto",
+        descripcion:"Aromatizante automatico de autos fragancia acqua",
+        precio: "$450.00"
     },
     {
         imagen:"Aromatizante en barra.webp",
-        titulo:"Aromatizante en barra"
+        titulo:"Aromatizante en barra",
+        descripcion:"Aromatizante con aroma floral",
+        precio: "$350.00"
     },
     {
         imagen:"lisoform.jpg",
-        titulo:"Lisoform"
+        titulo:"Lisoform",
+        descripcion:"Desinfectante de ambientes aroma lavanda",
+        precio: "$221.50"
     },
 ]
 module.exports ={
