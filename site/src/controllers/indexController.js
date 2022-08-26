dbcb693b8a307a4877143e865f26d3f22ee33b50
Romero713 <mauricioromerr2012@gmail.com>
2022-08-26 09:30:32 -0300

    dinamizo articulos en el index 25%

--- site/src/controllers/indexController.js
+++ site/src/controllers/indexController.js
@@ -1,6 +1,14 @@
+let aside= require('../data/aside.json')
+let productos = require('../data/productos.json')
 
-module.exports ={
-    home: (req,res) =>{
-        return res.render('index')
+
+module.exports = {
+    home : (req,res) => {
+        return res.render('index', 
+        {
+            mensaje: 'Aca estamos aprendiendo controladores',
+            aside,
+            productos
+        });
     }
 }
