31943ebac4a4ebeee0489268b1c23b2773064285
Romero713 <mauricioromerr2012@gmail.com>
2022-08-26 10:45:20 -0300

    dinamizo secciones 50%

--- site/src/controllers/productosControllers.js
+++ site/src/controllers/productosControllers.js
@@ -1,32 +1,14 @@
-let aside = [
-    {
-        imagen:"difusor.webp",
-        titulo:"Difusor automatico",
-        descripcion:"Aromatizante de ambientes con fragancia bebe",
-        precio: "$216.00"
-    },
-    {
-        imagen:"aqua fresh auto sport.jpg",
-        titulo:"Aromatizante de auto",
-        descripcion:"Aromatizante automatico de autos fragancia acqua",
-        precio: "$450.00"
-    },
-    {
-        imagen:"Aromatizante en barra.webp",
-        titulo:"Aromatizante en barra",
-        descripcion:"Aromatizante con aroma floral",
-        precio: "$350.00"
-    },
-    {
-        imagen:"lisoform.jpg",
-        titulo:"Lisoform",
-        descripcion:"Desinfectante de ambientes aroma lavanda",
-        precio: "$221.50"
-    },
-]
+let aside= require('../data/aside.json')
+let productos = require('../data/productos.json')
+
 module.exports ={
     detail: (req,res) =>{
-        return res.render('productDetail',{aside})
+        let idParams= +req.params.id;
+        let producto= productos.find(producto => {
+            return producto.id === idParams
+        } ) 
+        /* return res.send(productAencontrar) */
+        return res.render('productDetail',{ aside, producto} )
 
     },
     cart: (req,res) =>{
