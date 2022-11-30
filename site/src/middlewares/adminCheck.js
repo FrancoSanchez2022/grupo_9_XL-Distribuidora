aa562e896b2d3faf4c9713f88e6a404c33a303f4
FrancoSanchez2022 <fraannsanchez40@gmail.com>
2022-11-29 21:56:38 -0300

    Subo paginacion y buscador

--- site/src/middlewares/adminCheck.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res, next) => {
-    if (req.session.userLogin) {
-        if (res.locals.userLogin.rol === 'Admin') {
-            return next();
-        }
-    }
-    res.redirect('/')
-}
