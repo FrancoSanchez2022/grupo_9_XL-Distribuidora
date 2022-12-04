404f72788c9261f14897550ba9d3087c1644f138
Romero713 <mauricioromerr2012@gmail.com>
2022-12-04 17:30:02 -0300

    mejorando las validaciones al crear producto

--- site/public/javascripts/productos/crear.js
+++ site/public/javascripts/productos/crear.js
@@ -1,7 +1,26 @@
 window.addEventListener("load",()=> {
-    let $ = (elemento) => document.querySelector(elemento)
-    console.log("vinculadisimo");
+    
+    /* funcion para no aplicar document */
+    
+    const $ = (elemento) => document.querySelector(elemento)
+    /* console.log("vinculadisimo"); */
 
+    const funcValidate= (obj) =>{
+        let arr= Object.values(obj)
+        console.log(arr);
+        if (!arr.includes(false)) {
+            btn.disabled= false;
+            btn.style.backgroundColor= "#1a78fd"
+            } else {
+                btn.disabled= true;
+                btn.style.backgroundColor= "#545454"
+        }
+    }
+
+
+
+
+    /* elementos a validar */
     let titulo = $("#titulo")
     let precio= $("#precio")
     let descuento= $("#discount")
@@ -9,11 +28,30 @@ window.addEventListener("load",()=> {
     let categotia=$("#floatingSelect")
     let marca=$("#floatingSelectM")
     let descripcion=$("#floatingTextarea")
+    let img= $("#img-preview")
 
 
     titulo.addEventListener( "blur", ()=>{
-        console.log("me tocaste")
-        
+        /* console.log("me tocaste") */
+        switch (true) {
+            case !this.value.trim():
+                $("#tituloError").innerHTML= "debes ingresar el titulo del producto"
+                this.classList.add("is-invalid")
+                validate.titulo= false
+                break;
+            case !(this.value.trim().length > 2 && this.value.trim().length < 50 ):
+                $("#tituloError").innerHTML = "El titulo del producto debe contener 2 letras minimo y maximo 30"
+                this.classList.add("is-invalid")
+                validate.titulo= false
+                
+            default:
+                $("#tituloError").innerHTML = null
+                this.classList.remove("is-invalid")
+                this.classList.add("is-valid")
+                validate.titulo= true
+                break;
+        }
+        funcValidate(validate)
     })
     precio.addEventListener( "blur", ()=>{
         console.log("me tocaste")
